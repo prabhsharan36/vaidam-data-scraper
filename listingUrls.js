f0edf6d50004a124444e51daa009c28486b48a8a
prabhsharan36 <prabhsharan369@gmail.com>
2022-01-17 19:04:40 +0530

    changes

--- listingUrls.js
+++ listingUrls.js
@@ -1,26 +1,11 @@
 const puppeteer = require("puppeteer");
-// const log = (...args) => console.log(...args);
 (async () => {
   console.log("Started");
-
   const browser = await puppeteer.launch({
     headless: false,
     args: ["--no-sandbox"],
   });
-
-  //   const context = browser.defaultBrowserContext();
-  // await context.overridePermissions('https://www.swiggy.com', ['geolocation']);
-
   const page = await browser.newPage();
-
-  // await page.setGeolocation({ latitude: 21.1523063, longitude: 79.086033 });
-
-  // const cookiesString = await fs.readFile('./cookies.json');
-  // const cookies = JSON.parse(cookiesString);
-  // console.log(cookies)
-  // await page.setCookie(...cookies);
-
-  // await page.emulate(iPhone);
   let hospitalUrls = [];
   let pageNumber = 1;
   while (true) {
@@ -40,9 +25,8 @@ const puppeteer = require("puppeteer");
       pageNumber += 1;
     } else break;
   }
-
+  console.log("HospitalURLs", hospitalUrls);
   console.log("Total hospitalUrls: ", hospitalUrls.length);
-
   //   await browser.close();
   console.log("Finished");
 })();
