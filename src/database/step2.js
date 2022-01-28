79c8544e91af1a9988019ec45ec98584632758df
prabhsharan36 <prabhsharan369@gmail.com>
2022-01-28 21:20:53 +0530

    fix: minor changes

--- src/database/step2.js
+++ src/database/step2.js
@@ -7,7 +7,7 @@ MongoClient.connect(Url, async (err, client) => {
   console.log("✅ Database Connected");
   if (err) console.log("ERR in database connection: ", err);
   const db = client.db("vaidam-data");
-  const collection = db.collection("listingPageDoctors");
+  const collection = db.collection("doctorListingPages");
   let linksArr = [];
   let cursor = collection.find({});
   await cursor.forEach((doc) => {
@@ -18,17 +18,29 @@ MongoClient.connect(Url, async (err, client) => {
     let urlsData = await listingPageUrlsScraper(listingPageUrl);
     for (let index = 0; index < urlsData.urls.length; index++) {
       const doctorUrl = urlsData.urls[index];
-      const department = urlsData.department;
-      const treatment = urlsData.treatment;
-      const scrapedData = await doctorDataScraper(doctorUrl);
+      const departments = [urlsData.department];
+      const treatments = [urlsData.treatment];
       const collection = db.collection("doctorData");
-      const isSaved = await collection.insertOne({
-        ...scrapedData,
-        doctorUrl,
-        department,
-        treatment,
-      });
-      if (isSaved) console.log("Doctor Data Saved Successfully: ", index);
+      let doctor = collection.find({ doctorUrl: doctorUrl });
+      if (doctor) {
+        doctor.departments = [...doctor.departments, departments];
+        doctor.treatments = [...doctor.treatments, treatments];
+        const isUpdated = await collection.updateOne(
+          { doctorUrl: doctorUrl },
+          doctor
+        );
+        if (isUpdated) console.log("Doctor Updated Successfully: ", index);
+      } else {
+        const scrapedData = await doctorDataScraper(doctorUrl);
+        const collection = db.collection("doctorData");
+        const isSaved = await collection.insertOne({
+          ...scrapedData,
+          doctorUrl,
+          departments,
+          treatments,
+        });
+        if (isSaved) console.log("Doctor Data Saved Successfully: ", index);
+      }
     }
   }
 });
--- src/scrapers/doctorDataScraper.js
+++ src/scrapers/doctorDataScraper.js
@@ -19,20 +19,20 @@ async function doctorDataScraper(doctorUrl) {
     // );
     const DoctorData = await page.evaluate(() => {
       let doctorName = document.querySelector("h1.doc-name")?.innerText;
-      let doctorNameArr = doctorName.split(" ");
-      let index = doctorNameArr.indexOf("Dr.");
-      if (index === -1) index = doctorNameArr.indexOf("Dr");
-      if (index === -1) index = doctorNameArr.indexOf("Prof.");
-      if (index === -1) index = doctorNameArr.indexOf("Prof"); // vaidam sometimes miss "."
-      doctorNameArr = doctorNameArr.slice(index + 1);
+      let doctorNameArr = doctorName?.split(" ");
+      let index = doctorNameArr?.indexOf("Dr.");
+      if (index === -1) index = doctorNameArr?.indexOf("Dr");
+      if (index === -1) index = doctorNameArr?.indexOf("Prof.");
+      if (index === -1) index = doctorNameArr?.indexOf("Prof"); // vaidam sometimes miss "."
+      doctorNameArr = doctorNameArr?.slice(index + 1);
       let firstName = doctorNameArr?.[0];
       let middleName =
         doctorNameArr?.[1] && doctorNameArr?.[2] ? doctorNameArr?.[1] : null;
       let lastName = middleName ? doctorNameArr?.[2] : doctorNameArr?.[1];
       let city = document.querySelector("p.doc-location")?.innerText;
       if (city) {
-        city = city.split(",");
-        city = city.length > 0 ? city[0] : null;
+        city = city?.split(",");
+        city = city?.length > 0 ? city[0] : null;
       }
       city = city === "" ? null : city;
       let hospitalUrl = document.querySelector("#specialization h4 > a");
@@ -40,14 +40,14 @@ async function doctorDataScraper(doctorUrl) {
         hospitalUrl?.href === "https://www.vaidam.com/"
           ? null
           : hospitalUrl?.href;
-      const services = document.querySelector("#specialization p")?.innerText;
-      const specialization = document.querySelector(
-        ".doc-specialization h4"
-      )?.innerText;
+      const services = [document.querySelector("#specialization p")?.innerText];
+      const specializations = [
+        document.querySelector(".doc-specialization h4")?.innerText,
+      ];
       let education = document.querySelectorAll("div#education > ul > li");
-      education = [...education].map((item) => item?.innerText);
-      education = education.map((string) => {
-        let stringSplitted = string.split(", ");
+      education = [...education]?.map((item) => item?.innerText);
+      education = education?.map((string) => {
+        let stringSplitted = string?.split(", ");
         return {
           // "course": stringSplitted[0],
           college: stringSplitted[2] || null,
@@ -57,7 +57,7 @@ async function doctorDataScraper(doctorUrl) {
       let experiences = document.querySelectorAll(
         "div#section-workexperience > div.text-body > h4"
       );
-      experiences = [...experiences].map((item) => {
+      experiences = [...experiences]?.map((item) => {
         let splittedText = item?.innerText?.split(", ");
         const role =
           splittedText[0] && splittedText[0] !== " " ? splittedText[0] : null;
@@ -77,7 +77,7 @@ async function doctorDataScraper(doctorUrl) {
         education,
         experiences,
         services,
-        specialization,
+        specializations,
         hospitalUrl,
       };
     });
