42a649cf52006870a4f047cacd33767d3d8850bb
prabhsharan36 <prabhsharan369@gmail.com>
2022-01-31 12:24:07 +0530

    fixes

--- src/database/step2.js
+++ src/database/step2.js
@@ -1,5 +1,6 @@
 /* Saving Hospitals Urls */
 
+const { update } = require("lodash");
 const hospitalListingPageUrlsScraper = require("../scrapers/hospitalListingPageUrlsScraper");
 const MongoClient = require("mongodb").MongoClient;
 const Url = "mongodb://localhost:27017/Scraper";
@@ -16,7 +17,7 @@ MongoClient.connect(Url, async (err, client) => {
   for (let index = 0; index < urlsArr.length; index++) {
     const obj = urlsArr[index];
     const department = obj.department;
-    const treatment = obj.treatment;
+    // const treatment = obj.treatment;
     const treatmentListingUrl = obj.treatmentListingUrl;
     const hospitalUrls = await hospitalListingPageUrlsScraper(
       treatmentListingUrl
@@ -26,38 +27,42 @@ MongoClient.connect(Url, async (err, client) => {
       const url = hospitalUrls[index];
       const hospital = await collection.findOne({ url });
       if (hospital) {
-        let isDepartmentAlreadyPresent = false;
-        for (let index = 0; index < hospital.meta.length; index++) {
-          const obj = hospital.meta[index];
-          if (obj.department === department) {
-            isDepartmentAlreadyPresent = true;
-            obj.treatments.push(treatment);
-          }
+        const isUpdated = false;
+        // let isDepartmentAlreadyPresent = false;
+        if (hospital.departments.indexOf(department) < 0) {
+          hospital.departments.push(department);
+          isUpdated = true;
         }
-        if (!isDepartmentAlreadyPresent) {
-          // when it was not present so we add new Department and it's treament
-          hospital.meta.push({
-            department,
-            treatments: [treatment],
-          });
-        }
+        // for (let index = 0; index < hospital.length; index++) {
+        //   const obj = hospital.meta[index];
+        //   if (obj.department === department) {
+        //     isDepartmentAlreadyPresent = true;
+        //     if (obj.treatments.indexOf(treatment) >= 0)
+        //       // only add if not present
+        //       obj.treatments.push(treatment);
+        //   }
+        // }
+        // if (!isDepartmentAlreadyPresent) {
+        //   // when it was not present so we add new Department and it's treament
+        //   hospital.meta.push({
+        //     department,
+        //     treatments: [treatment],
+        //   });
+        // }
         // const newDepartments = [...hospital.departments, ...departments];
         // const newTreatments = [...hospital.treatments, ...treatments];
-        const isUpdated = await collection.updateOne(
-          { url },
-          { $set: { meta: hospital.meta } }
-        );
-        if (isUpdated) console.log("Hospital Updated Successfully");
+        if (isUpdated) {
+          const result = await collection.updateOne(
+            { url },
+            { $set: { departments: hospital.departments } }
+          );
+          if (result) console.log("Hospital Updated Successfully");
+        }
       } else {
         collection
           .insertOne({
             url,
-            meta: [
-              {
-                department,
-                treatments: [treatment],
-              },
-            ],
+            departments: [department],
           })
           .then(() => {
             console.log("New Hospital added successfully in Database!!!!");
