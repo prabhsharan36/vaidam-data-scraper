40f838b8b160e5af38db8e0b82abc45aad90e1b3
prabhsharan36 <prabhsharan369@gmail.com>
2022-01-31 18:18:16 +0530

    changes

--- src/database/fetchDoctorSpecializations.js
+++ src/database/fetchDoctorSpecializations.js
@@ -3,21 +3,25 @@
 const MongoClient = require("mongodb").MongoClient;
 const Url = "mongodb://localhost:27017/Scraper";
 MongoClient.connect(Url, async (err, client) => {
-  console.log("✅ Database Connected");
-  const db = client.db("vaidam-data");
-  const DoctorDataColl = db.collection("doctorData");
-  let specializations = [];
-  let cursor = DoctorDataColl.find({});
-  while (await cursor.hasNext()) {
-    let doctorData = await cursor.next();
-    const docSpecializations = doctorData?.specializations;
-    if (docSpecializations.length > 0)
-      specializations.push(...docSpecializations); // first add all specializations and then remove duplicated items
+  try {
+    console.log("✅ Database Connected");
+    const db = client.db("vaidam-data");
+    const DoctorDataColl = db.collection("doctorData");
+    let specializations = [];
+    let cursor = DoctorDataColl.find({});
+    while (await cursor.hasNext()) {
+      let doctorData = await cursor.next();
+      const docSpecializations = doctorData?.specializations;
+      if (docSpecializations.length > 0)
+        specializations.push(...docSpecializations); // first add all specializations and then remove duplicated items
+    }
+    specializations = specializations.filter(function (specialization, pos) {
+      return specializations.indexOf(specialization) == pos;
+    });
+    console.log(specializations);
+    if (err) console.log("ERR in database connection: ", err);
+    console.log("Finished: fetchDoctorSpecializations");
+  } catch (err) {
+    console.log(err);
   }
-  specializations = specializations.filter(function (specialization, pos) {
-    return specializations.indexOf(specialization) == pos;
-  });
-  console.log(specializations);
-  if (err) console.log("ERR in database connection: ", err);
-  console.log("Finished: fetchDoctorSpecializations");
 });
---
+++ src/database/fetchHospitalDepartments.js
@@ -0,0 +1,27 @@
+/* Fetch Doctors Specializations */
+
+const MongoClient = require("mongodb").MongoClient;
+const Url = "mongodb://localhost:27017/Scraper";
+MongoClient.connect(Url, async (err, client) => {
+  try {
+    console.log("✅ Database Connected");
+    const db = client.db("vaidam-data");
+    const HospitalPageUrlColl = db.collection("hospitalPageUrls");
+    let departments = [];
+    let cursor = HospitalPageUrlColl.find({});
+    while (await cursor.hasNext()) {
+      let hospitalData = await cursor.next();
+      const hospitalDepartments = hospitalData?.departments;
+      if (hospitalDepartments.length > 0)
+        departments.push(...hospitalDepartments); // first add all departments and then remove duplicated items
+    }
+    departments = departments.filter(function (department, pos) {
+      return departments.indexOf(department) == pos;
+    });
+    console.log(departments);
+    if (err) console.log("ERR in database connection: ", err);
+    console.log("Finished: fetchHospitalDepartments");
+  } catch (err) {
+    console.log(err);
+  }
+});
